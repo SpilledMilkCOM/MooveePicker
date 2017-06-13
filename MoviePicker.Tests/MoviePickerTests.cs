fe0ac3f75d6ac9dd1111fa3e586ce827fc88ccfb
Parker Smart <Parker.Smart@ncm.com>
2017-06-13 11:25:40 -0600

    Added WeekEnding_2017061 test. (with actuals from Box Office Mojo)

--- MoviePicker.Tests/MoviePickerTests.cs
+++ MoviePicker.Tests/MoviePickerTests.cs
@@ -196,25 +196,6 @@ namespace MoviePicker.Tests
 			movies.Add(ConstructMovie(id++, "Snatched", 1.3m, 14));
 			movies.Add(ConstructMovie(id++, "King Arthur", 1.2m, 12));
 
-			// From Raj
-			//movies.Add(ConstructMovie(id++, "Wonder Woman", 103.25m, 845));
-			//movies.Add(ConstructMovie(id++, "Captain Underpants", 23.9m, 239));
-			//movies.Add(ConstructMovie(id++, "Pirates of the Caribbean: Dead Men Tell No Tales", 22.1m, 193));
-			//movies.Add(ConstructMovie(id++, "Guardians of the Galaxy Vol. 2", 9.8m, 74));
-			//movies.Add(ConstructMovie(id++, "Baywatch", 8.7m, 62));
-			//movies.Add(ConstructMovie(id++, "Alien: Covenant", 4.1m, 31));
-			//movies.Add(ConstructMovie(id++, "Everything Everything", 3.3m, 22));
-			//movies.Add(ConstructMovie(id++, "Diary of a Wimpy Kid: The Long Haul", 1.3m, 17));
-			//movies.Add(ConstructMovie(id++, "Snatched", 1.3m, 14));
-			//movies.Add(ConstructMovie(id++, "King Arthur: Legend of the Sword", 1.2m, 12));
-
-			// These movies seem inconsequential.
-			//movies.Add(ConstructMovie(id++, "The Mummy", 38, 526));
-			//movies.Add(ConstructMovie(id++, "It Comes at Night", 20, 150));
-			//movies.Add(ConstructMovie(id++, "Meagan Leavey", 3.3m, 59));
-			//movies.Add(ConstructMovie(id++, "My Cousin Rachel", 1, 15));
-			//movies.Add(ConstructMovie(id++, "Best of the Rest", 1.1m, 9));
-
 			test.AddMovies(movies);
 
 			var best = test.ChooseBest();
@@ -224,6 +205,44 @@ namespace MoviePicker.Tests
 
 			Assert.AreEqual(1, best.Movies.Count(movie => movie.Name == "Wonder Woman"));
 			Assert.AreEqual(7, best.Movies.Count(movie => movie.Name == "Everything Everything"));
+		}
+
+		[TestMethod]
+		public void MoviePicker_ChooseBest_WeekEnding_20170611()
+		{
+			var test = ConstructTestObject();
+			var movies = new List<IMovie>();
+			int id = 1;
+
+			// Weekend gross from box office mojo (with FML bux)
+ 
+			movies.Add(ConstructMovie(id++, "Wonder Woman", 58520672m, 613));
+			movies.Add(ConstructMovie(id++, "The Mummy", 31688375m, 526));
+			movies.Add(ConstructMovie(id++, "It Comes at night", 5988370m, 150));
+			movies.Add(ConstructMovie(id++, "Pirates of the caribbean", 10704103m, 143));
+			movies.Add(ConstructMovie(id++, "Baywatch", 4648207m, 60));
+			movies.Add(ConstructMovie(id++, "Megan Leavey", 3810867m, 59));
+			movies.Add(ConstructMovie(id++, "Alien: Covenant", 1826579m, 26));
+			movies.Add(ConstructMovie(id++, "My Cousin Rachel", 968506m, 15));
+			movies.Add(ConstructMovie(id++, "Diary of a wimpy Kid", 656843m, 8));
+			movies.Add(ConstructMovie(id++, "Captain Underpants", 12180704m, 198));
+			movies.Add(ConstructMovie(id++, "Everything, Everything", 1627295m, 28));
+			movies.Add(ConstructMovie(id++, "Guardians of the Galaxy", 6312367m, 70));
+			movies.Add(ConstructMovie(id++, "King Arthur", 455744m, 7));
+			movies.Add(ConstructMovie(id++, "Snatched", 491644m, 9));
+			movies.Add(ConstructMovie(id++, "Best of the rest", 512445m, 9));
+
+			test.AddMovies(movies);
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(1, best.Movies.Count(movie => movie.Name == "Wonder Woman"));
+			Assert.AreEqual(5, best.Movies.Count(movie => movie.Name == "Guardians of the Galaxy"));
+			Assert.AreEqual(1, best.Movies.Count(movie => movie.Name == "Alien: Covenant"));
+			Assert.AreEqual(1, best.Movies.Count(movie => movie.Name == "Diary of a wimpy Kid"));
 		}
 
 		[TestMethod]
