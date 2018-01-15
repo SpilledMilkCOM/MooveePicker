3c76fd3b3edc4c97701aca842ec0322be6ba7d6f
Parker Smart <psmart@spilledmilk.com>
2018-01-15 16:37:53 -0700

    Using reflection to reduce the code imprint of the Box Office values.

---
+++ MoviePicker.WebApp/Models/ReflectionUtility.cs
@@ -0,0 +1,12 @@
+﻿namespace MoviePicker.WebApp.Models
+{
+	public static class ReflectionUtility
+	{
+		public static object GetValue(object obj, string propertyName)
+		{
+			//TODO: Store the info statically so it only has to be done once.
+
+			return obj.GetType().GetProperty(propertyName)?.GetValue(obj);
+		}
+	}
+}
