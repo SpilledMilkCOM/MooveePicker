43174b2c4943919218833484e53484e22daeb0e8
Parker Smart <psmart@spilledmilk.com>
2018-01-24 07:15:00 -0700

    Stub for DownloadFiles()

---
+++ MoviePicker.WebApp/Utilities/FileUtility.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace MoviePicker.WebApp.Utilities
+{
+	public static class FileUtility
+	{
+		/// <summary>
+		/// Download a list of files.
+		/// </summary>
+		/// <param name="files">A list of file Urls</param>
+		/// <param name="localFilePrefix">Full path plus local file prefix</param>
+		public static void DownloadFiles(List<string> files, string localFilePrefix)
+		{
+			// Loop through the files.
+
+			// Verify the file doesn't already exist.
+
+
+		}
+	}
+}
