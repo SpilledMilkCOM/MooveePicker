e9d7911af2c979592bffab1870369521ec2c02e0
Parker Smart <psmart@spilledmilk.com>
2018-02-05 21:12:41 -0700

    Updated Cow logo.  Updated some of the mobile view.

--- MovieMiner/MineBoxOfficeMojo.cs
+++ MovieMiner/MineBoxOfficeMojo.cs
@@ -22,7 +22,7 @@ namespace MovieMiner
 		/// <param name="weekendEnding">If this is null then the forecast will be mined.</param>
 		public MineBoxOfficeMojo(DateTime? weekendEnding = null)
 			: base($"Box Office Mojo {weekendEnding?.ToShortDateString()}"
-				  , $"BO Mojo{weekendEnding?.ToShortDateString()}", DEFAULT_URL)
+				  , $"BO Mojo {weekendEnding?.ToShortDateString()}", DEFAULT_URL)
 		{
 			_weekendEnding = weekendEnding?.Date;
 		}
