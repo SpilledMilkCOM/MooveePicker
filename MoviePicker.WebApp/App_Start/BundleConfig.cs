187a8a7ff35d823177fdc92ac8a45e7d71905357
Parker Smart <psmart@spilledmilk.com>
2018-08-02 18:25:06 -0600

    Rolled back to bootstrap 3.3.7

--- MoviePicker.WebApp/App_Start/BundleConfig.cs
+++ MoviePicker.WebApp/App_Start/BundleConfig.cs
@@ -15,7 +15,8 @@ namespace MoviePicker.WebApp
 			// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
 
-			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
+			//bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/popper.js", "~/Scripts/respond.js"));
+			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
 			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
 			bundles.Add(new ScriptBundle("~/bundles/utilities").Include("~/Scripts/utilities.js"));
 		}
