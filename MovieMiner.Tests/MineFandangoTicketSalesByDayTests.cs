37ac7b59b6c345cd6108021d8281db6bb27d8060
Parker Smart <psmart@spilledmilk.com>
2018-10-01 09:30:22 -0600

    Adding Fandango by day.

---
+++ MovieMiner.Tests/MineFandangoTicketSalesByDayTests.cs
@@ -0,0 +1,241 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoviePicker.Common;
+using MoviePicker.Common.Interfaces;
+using MoviePicker.Msf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Unity;
+
+namespace MovieMiner.Tests
+{
+	[TestClass]
+	[ExcludeFromCodeCoverage]
+	public class MineFandangoTicketSalesByDayTests : MineTestBase
+	{
+		// Unity Reference: https://msdn.microsoft.com/en-us/library/ff648211.aspx
+		private static IUnityContainer _unity;
+
+		public override IUnityContainer UnityContainer => _unity;
+
+		[ClassInitialize]
+		public static void InitializeBeforeAllTests(TestContext context)
+		{
+			_unity = new UnityContainer();
+
+			_unity.RegisterType<ILogger, DebugLogger>();
+			_unity.RegisterType<IMoviePicker, MsfMovieSolver>();
+		}
+
+		[TestMethod, TestCategory(PRIMARY_TEST_CATEGORY), TestCategory("Single")]
+		public void MineFandangoTicketSales_Mine()
+		{
+			var test = new MineFandangoTicketSales();
+
+			var actual = test.Mine();
+
+			Assert.IsNotNull(actual);
+			Assert.IsTrue(actual.Any(), "The list was empty.");
+		}
+
+		[TestMethod, TestCategory(PRIMARY_TEST_CATEGORY), TestCategory("Single")]
+		public void MineFandangoTicketSales_Totals()
+		{
+			var test = new MineFandangoTicketSales();
+
+			var actual = test.Mine();
+
+			Assert.IsNotNull(actual);
+			Assert.IsTrue(actual.Any(), "The list was empty.");
+
+			actual = actual.GroupBy(movie => movie.Name)
+							.Select(group => new Movie { Name = group.Key, Earnings = group.Sum(item => item.Earnings) })
+							.Cast<IMovie>()
+							.Where(movie => movie.Earnings > 1000)
+							.OrderByDescending(movie => movie.Earnings)
+							.ToList();
+
+			WriteMovies(actual);
+		}
+
+		[TestMethod, TestCategory(PRIMARY_TEST_CATEGORY), TestCategory("Single")]
+		public void MineFandangoTicketSales_TotalsCompressed()
+		{
+			var test = new MineFandangoTicketSales();
+			var fmlMiner = new MineFantasyMovieLeagueBoxOffice();
+
+			var actual = test.Mine();
+
+			Assert.IsNotNull(actual);
+			Assert.IsTrue(actual.Any(), "The list was empty.");
+
+			var gameMovies = fmlMiner.Mine();
+
+			Assert.IsNotNull(gameMovies);
+			Assert.IsTrue(gameMovies.Any(), "The game list was empty.");
+
+			actual = actual.GroupBy(movie => movie.Name)
+							.Select(group => new Movie { Name = group.Key, Earnings = group.Sum(item => item.Earnings) })
+							.Cast<IMovie>()
+							.OrderByDescending(movie => movie.Earnings)
+							.ToList();
+
+			actual = actual.Where(movie => gameMovies.Contains(movie)).ToList();
+
+			var copy = new List<IMovie>(actual);
+			var toRemove = new List<IMovie>();
+
+			foreach (var movie in actual)
+			{
+				// Find a simlarly named movie
+
+				var likeMovie = copy.FirstOrDefault(item => item.Equals(movie) && item.MovieName != movie.MovieName);
+
+				if (likeMovie != null && !toRemove.Contains(likeMovie))
+				{
+					// Add the totals.
+
+					movie.Earnings += likeMovie.Earnings;
+
+					// Remove the movie so it can't be found again.
+
+					RemoveSameName(copy, likeMovie);
+					toRemove.Add(likeMovie);
+				}
+			}
+
+			foreach (var movie in toRemove)
+			{
+				var found = actual.FirstOrDefault(item => item.MovieName == movie.MovieName);
+
+				if (found != null)
+				{
+					RemoveSameName(actual, found);
+				}
+			}
+
+			// Assign the cost for WriteMovies to compute Efficiency.
+
+			var now = DateTime.Now;
+
+			foreach (var movie in actual)
+			{
+				var found = gameMovies.FirstOrDefault(item => item.Equals(movie));
+
+				if (found != null)
+				{
+					movie.Cost = found.Cost;
+					movie.WeekendEnding = now;
+				}
+			}
+
+			WriteMovies(actual, true);
+		}
+
+		[TestMethod, TestCategory(PRIMARY_TEST_CATEGORY), TestCategory("Single")]
+		public void MineFandangoTicketSales_TotalsCompressed_MakePicks()
+		{
+			var test = new MineFandangoTicketSales();
+			var fmlMiner = new MineFantasyMovieLeagueBoxOffice();
+
+			var actual = test.Mine();
+
+			Assert.IsNotNull(actual);
+			Assert.IsTrue(actual.Any(), "The list was empty.");
+
+			var gameMovies = fmlMiner.Mine();
+
+			Assert.IsNotNull(gameMovies);
+			Assert.IsTrue(gameMovies.Any(), "The game list was empty.");
+
+			var now = DateTime.Now;
+
+			actual = actual.Where(movie => movie.WeekendEnding > now.AddHours(-24))
+							.GroupBy(movie => movie.Name)
+							.Select(group => new Movie { Name = group.Key, Earnings = group.Sum(item => item.Earnings) })
+							.Cast<IMovie>()
+							.OrderByDescending(movie => movie.Earnings)
+							.ToList();
+
+			actual = actual.Where(movie => gameMovies.Contains(movie)).ToList();
+
+			var copy = new List<IMovie>(actual);
+			var toRemove = new List<IMovie>();
+
+			foreach (var movie in actual)
+			{
+				// Find a simlarly named movie
+
+				var likeMovie = copy.FirstOrDefault(item => item.Equals(movie) && item.MovieName != movie.MovieName);
+
+				if (likeMovie != null && !toRemove.Contains(likeMovie))
+				{
+					// Add the totals.
+
+					movie.Earnings += likeMovie.Earnings;
+
+					// Remove the movie so it can't be found again.
+
+					RemoveSameName(copy, likeMovie);
+					toRemove.Add(likeMovie);
+				}
+			}
+
+			foreach (var movie in toRemove)
+			{
+				var found = actual.FirstOrDefault(item => item.MovieName == movie.MovieName);
+
+				if (found != null)
+				{
+					RemoveSameName(actual, found);
+				}
+			}
+
+			// Assign the cost for WriteMovies to compute Efficiency.
+
+			foreach (var movie in actual)
+			{
+				var found = gameMovies.FirstOrDefault(item => item.Equals(movie));
+
+				if (found != null)
+				{
+					movie.Cost = found.Cost;
+					movie.WeekendEnding = now;
+				}
+			}
+
+			WriteMovies(actual, true);
+
+			var moviePicker = _unity.Resolve<IMoviePicker>();
+
+			moviePicker.AddMovies(actual);
+			//moviePicker.EnableBestPerformer = false;
+
+			var bonusPicks = moviePicker.ChooseBest();
+
+			WritePicker(moviePicker);
+			WriteMovies(bonusPicks);
+		}                                           
+
+		private void RemoveSameName(List<IMovie> list, IMovie toRemove)
+		{
+			var indexToRemove = 0;
+
+			foreach (var movie in list)
+			{
+				if (movie.MovieName == toRemove.MovieName)
+				{
+					break;
+				}
+
+				indexToRemove++;
+			}
+
+			if (indexToRemove < list.Count)
+			{
+				list.RemoveAt(indexToRemove);
+			}
+		}
+	}
+}
---
+++ MovieMiner/MineFandangoTicketSalesByDay.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MoviePicker.Common;
+using MoviePicker.Common.Interfaces;
+
+namespace MovieMiner
+{
+	public class MineFandangoTicketSalesByDay : MinerBase
+	{
+		private const decimal AVERAGE_COST_PER_TICKET = 10;
+
+		private const string DEFAULT_URL = "http://akvalley.pythonanywhere.com/static/Fandango_report.txt";
+		private const string DELIMITER = "- $";
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="weekendEnding">If this is null then the forecast will be mined.</param>
+		public MineFandangoTicketSalesByDay()
+			: base("Fandango Tickets Sales (by day)", "Fandango Dailies", DEFAULT_URL)
+		{
+		}
+
+		public override IMiner Clone()
+		{
+			return null;
+		}
+
+		public override List<IMovie> Mine()
+		{
+			var lineDelimiters = new[] { '\n' };
+			var tokenDelimiters = new[] { '\t' };
+			var result = new List<IMovie>();
+			var data = HttpRequestUtil.DownloadTextFile(Url);
+			var lines = data?.Split(lineDelimiters);
+
+			if (lines != null)
+			{
+				foreach (var line in lines.Skip(3))
+				{
+					var tokens = line?.Split(tokenDelimiters);
+
+					if (tokens != null && tokens.Length == 3)
+					{
+						var movie = new Movie
+						{
+							WeekendEnding = Convert.ToDateTime(tokens[0]),
+							Earnings = Convert.ToDecimal(tokens[1]) * AVERAGE_COST_PER_TICKET,
+							Name = tokens[2]
+						};
+						result.Add(movie);
+					}
+				}
+			}
+
+			return result;
+		}
+	}
+}
