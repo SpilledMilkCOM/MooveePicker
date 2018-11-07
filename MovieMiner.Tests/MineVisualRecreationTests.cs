fd4679a14dc71fa1411fe983608c2d8c7b3954f7
Parker Smart <psmart@spilledmilk.com>
2018-11-07 13:52:13 -0700

    Added Vis Rec data.  A first time for everything. TODO: Get transcript and attempt to parse it for dollar values.

---
+++ MovieMiner.Tests/MineVisualRecreationTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoviePicker.Common.Interfaces;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Unity;
+
+namespace MovieMiner.Tests
+{
+	[TestClass]
+	[ExcludeFromCodeCoverage]
+	public class MineVisualRecreationTests : MineTestBase
+	{
+		// Unity Reference: https://msdn.microsoft.com/en-us/library/ff648211.aspx
+		private static IUnityContainer _unity;
+
+		public override IUnityContainer UnityContainer => _unity;
+
+		[ClassInitialize]
+		public static void InitializeBeforeAllTests(TestContext context)
+		{
+			_unity = new UnityContainer();
+
+			_unity.RegisterType<ILogger, DebugLogger>();
+		}
+
+		[TestMethod, TestCategory(PRIMARY_TEST_CATEGORY), TestCategory("Single")]
+		public void MineVisualRecreation_Mine_GenerateBoxOfficeValues()
+		{
+			// http://pro.boxoffice.com/
+
+			var test = new MineFantasyMovieLeagueBoxOffice();
+
+			var actual = test.Mine();
+
+			Assert.IsNotNull(actual);
+			Assert.IsTrue(actual.Any(), "The list was empty.");
+
+			var weekendEnding = actual[0].WeekendEnding;
+			var tab = "\t";
+
+			Logger.WriteLine($"{tab}{tab}{tab}var weekend = new DateTime({weekendEnding.Year}, {weekendEnding.Month}, {weekendEnding.Day});");
+			Logger.WriteLine($"{tab}{tab}{tab}UrlSource = \"{test.UrlSource}\";");
+			Logger.WriteLine($"{tab}{tab}{tab}return new List<IMovie>");
+			Logger.WriteLine($"{tab}{tab}{tab}{tab}{tab}{tab}{{");
+
+			foreach (var movie in actual.OrderByDescending(item => item.Cost))
+			{
+				Logger.WriteLine($"{tab}{tab}{tab}{tab}{tab}{tab}{tab}{tab}new Movie {{ MovieName = \"{movie.MovieName}\", Earnings = 0, WeekendEnding = weekend }},");
+			}
+			Logger.WriteLine($"{tab}{tab}{tab}{tab}{tab}{tab}}};");
+		}
+	}
+}
---
+++ MovieMiner/MineVisualRecreation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+using MoviePicker.Common.Interfaces;
+using MoviePicker.Common;
+
+namespace MovieMiner
+{
+	public class MineVisualRecreation : MinerBase
+	{
+		private const string DEFAULT_URL = "https://www.youtube.com/channel/UCy3oMGqYJiPWQ030mYbaZig";
+
+		private bool _mineData = false;
+		//private bool _mineData = true;
+
+		public MineVisualRecreation()
+			: base("Visual Recreation", "Vis Rec", DEFAULT_URL)
+		{
+			TwitterID = "BoxOffice";
+		}
+
+		public override IMiner Clone()
+		{
+			var result = new MineBoxOfficePro();
+
+			Clone(result);
+
+			return result;
+		}
+
+		public override List<IMovie> Mine()
+		{
+			if (_mineData)
+			{
+				return MineData();
+			}
+			else
+			{
+				var weekend = new DateTime(2018, 11, 11);
+				UrlSource = "https://www.youtube.com/watch?v=Ahav1u-rDlo";
+				return new List<IMovie>
+					{
+						new Movie { MovieName = "The Grinch", Earnings = 71000000, WeekendEnding = weekend },
+						new Movie { MovieName = "Bohemian Rhapsody", Earnings = 32000000, WeekendEnding = weekend },
+						new Movie { MovieName = "The Girl in the Spiders Web", Earnings = 10500000, WeekendEnding = weekend },
+						new Movie { MovieName = "The Nutcracker  the Four Realms", Earnings = 10800000, WeekendEnding = weekend },
+						new Movie { MovieName = "Overlord", Earnings = 8000000, WeekendEnding = weekend },
+						new Movie { MovieName = "A Star Is Born", Earnings = 7400000, WeekendEnding = weekend },
+						new Movie { MovieName = "Nobodys Fool", Earnings = 6300000, WeekendEnding = weekend },
+						new Movie { MovieName = "Venom", Earnings = 5000000, WeekendEnding = weekend },
+						new Movie { MovieName = "Halloween", Earnings = 4300000, WeekendEnding = weekend },
+						new Movie { MovieName = "Smallfoot", Earnings = 2000000, WeekendEnding = weekend },
+					};
+			}
+		}
+
+		private List<IMovie> MineData()
+		{
+			return null;
+		}
+	}
+}
