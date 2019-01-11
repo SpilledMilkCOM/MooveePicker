f312e2e5ee93e46c6cb25bc8e10b2ad53702b1a5
Parker Smart <psmart@spilledmilk.com>
2019-01-11 08:29:04 -0700

    Adding Box Office object.

---
+++ MoviePicker.Common/BoxOffice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MoviePicker.Common.Interfaces;
+
+namespace MoviePicker.Common
+{
+	public class BoxOffice : IBoxOffice
+	{
+		public decimal Earnings { get; set; }
+
+		int Rank { get; set; }
+
+		public int TheaterCount { get; set; }
+
+		public DateTime WeekendEnding { get; set; }
+	}
+}
---
+++ MoviePicker.Common/Interfaces/IBoxOffice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MoviePicker.Common.Interfaces
+{
+	public interface IBoxOffice
+	{
+		Decimal Earnings { get; set; }
+
+		int Rank { get; set; }
+
+		int TheaterCount { get; set; }
+
+		DateTime WeekendEnding { get; set; }
+	}
+}
