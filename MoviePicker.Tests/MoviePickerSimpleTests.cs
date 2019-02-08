da6fca8bbf52f0c9660d129a854ca18e912980d0
Parker Smart <psmart@spilledmilk.com>
2019-02-07 21:25:19 -0700

    Added some simple tests.

---
+++ MoviePicker.Tests/MoviePickerSimpleTests.cs
@@ -0,0 +1,282 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoviePicker.Common;
+using MoviePicker.Common.Interfaces;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Unity;
+
+namespace MoviePicker.Tests
+{
+	[TestClass]
+	[ExcludeFromCodeCoverage]
+	public class MoviePickerSimpleTests : MoviePickerTestBase
+	{
+		// Unity Reference: https://msdn.microsoft.com/en-us/library/ff648211.aspx
+		private static IUnityContainer _unity;
+
+		public override IUnityContainer UnityContainer => _unity;
+
+		[ClassInitialize]
+		public static void InitializeBeforeAllTests(TestContext context)
+		{
+			_unity = new UnityContainer();
+
+			_unity.RegisterType<IMovie, Movie>();
+			_unity.RegisterType<IMovieList, MovieList>();
+			_unity.RegisterType<IMoviePicker, MooveePicker.MoviePicker>();
+		}
+
+		[TestMethod]
+		public void MoviePicker_ChooseBest_OutOf01()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(1).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(2, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePicker_ChooseBest_OutOf02()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(2).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(4, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePicker_ChooseBest_OutOf03()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(3).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(7, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePicker_ChooseBest_OutOf04()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(4).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(8, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePicker_ChooseBest_OutOf05()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(5).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(7, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePicker_ChooseBest_OutOf06()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(6).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(7, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePicker_ChooseBest_OutOf07()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(7).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(7, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePicker_ChooseBest_OutOf08()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(8).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(7, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePicker_ChooseBest_OutOf09()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(9).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(7, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePicker_ChooseBest_OutOf10()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(10).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(7, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePicker_ChooseBest_OutOf11()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(11).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(7, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePicker_ChooseBest_OutOf12()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(12).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(7, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePicker_ChooseBest_OutOf13()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(13).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(7, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePicker_ChooseBest_OutOf14()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(14).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(7, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePicker_ChooseBest_OutOf15()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(15).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(8, best.Movies.Count());
+		}
+
+		//----==== PRIVATE ====---------------------------------------------------------
+
+		private List<IMovie> ThisWeeksMoviesPicks()
+		{
+			var movies = new List<IMovie>();
+
+			int id = 1;
+
+			movies.Add(ConstructMovie(id++, "AAA", 50.0m, 500));
+			movies.Add(ConstructMovie(id++, "BBB", 25.0m, 250));
+			movies.Add(ConstructMovie(id++, "CCC", 10.0m, 100));
+			movies.Add(ConstructMovie(id++, "DDD", 5.1m, 50));
+			movies.Add(ConstructMovie(id++, "EEE", 5.0m, 100));
+			movies.Add(ConstructMovie(id++, "FFF", 5.0m, 100));
+			movies.Add(ConstructMovie(id++, "GGG", 5.0m, 100));
+			movies.Add(ConstructMovie(id++, "HHH", 5.0m, 100));
+			movies.Add(ConstructMovie(id++, "III", 5.0m, 100));
+			movies.Add(ConstructMovie(id++, "JJJ", 5.0m, 100));
+			movies.Add(ConstructMovie(id++, "KKK", 5.0m, 100));
+			movies.Add(ConstructMovie(id++, "LLL", 5.0m, 100));
+			movies.Add(ConstructMovie(id++, "MMM", 5.0m, 100));
+			movies.Add(ConstructMovie(id++, "NNN", 5.0m, 100));
+			movies.Add(ConstructMovie(id++, "OOO", 10.1m, 100));
+
+			return movies;
+		}
+	}
+}
