2f388b015c4f9e2f03a57f44d0dfbf061a1aef71
Parker Smart <psmart@spilledmilk.com>
2019-03-14 19:32:46 -0600

    BO pro changed their table attributes.

--- MovieMiner/MineBoxOfficePro.cs
+++ MovieMiner/MineBoxOfficePro.cs
@@ -105,7 +105,8 @@ namespace MovieMiner
 					// Get the data in the table.
 
 					//node = doc.DocumentNode.SelectSingleNode("//body//div[@class='post-container']/table");
-					node = doc.DocumentNode.SelectSingleNode("//body//table[@class='wp-block-table aligncenter']");
+					//node = doc.DocumentNode.SelectSingleNode("//body//table[@class='wp-block-table aligncenter']");
+					node = doc.DocumentNode.SelectSingleNode("//body//table[@width='726']");
 
 					// TODO: Parse the header for column titles for mapping.
 
