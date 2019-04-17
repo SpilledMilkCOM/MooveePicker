e9ba7c4c55dd717a47d8169af1443905a8ed939f
Parker Smart <psmart@spilledmilk.com>
2019-04-16 21:42:25 -0600

    Added Unbounded Knapsack movie picker.

---
+++ MoviePicker.Tests/MoviePickerUnboundedKnapsackTests.cs
@@ -0,0 +1,342 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoviePicker.Common;
+using MoviePicker.Common.Interfaces;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Unity;
+
+namespace MoviePicker.Tests
+{
+	[TestClass]
+	[ExcludeFromCodeCoverage]
+	public class MoviePickerUnboundedKnapsackTests : MoviePickerTestBase
+	{
+		// Unity Reference: https://msdn.microsoft.com/en-us/library/ff648211.aspx
+		private static IUnityContainer _unity;
+
+		public override IUnityContainer UnityContainer => _unity;
+
+		[ClassInitialize]
+		public static void InitializeBeforeAllTests(TestContext context)
+		{
+			_unity = new UnityContainer();
+
+			_unity.RegisterType<IMovie, Movie>();
+			_unity.RegisterType<IMovieList, MovieList>();
+			_unity.RegisterType<IMoviePicker, MooveePicker.MoviePickerUnboundedKnapsack>();
+		}
+
+		[TestMethod]
+		public void MoviePickerUnboundedKnapsack_ChooseBest_OutOf01()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(1).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(1, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePickerUnboundedKnapsack_ChooseBest_OutOf02()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(2).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(1, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePickerUnboundedKnapsack_ChooseBest_OutOf03()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(3).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(2, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePickerUnboundedKnapsack_ChooseBest_OutOf04()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(4).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(6, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePickerUnboundedKnapsack_ChooseBest_OutOf05()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(5).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(6, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePickerUnboundedKnapsack_ChooseBest_OutOf06()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(6).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(7, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePickerUnboundedKnapsack_ChooseBest_OutOf07()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(7).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(7, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePickerUnboundedKnapsack_ChooseBest_OutOf08()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(8).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(7, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePickerUnboundedKnapsack_ChooseBest_OutOf09()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(9).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(8, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePickerUnboundedKnapsack_ChooseBest_OutOf10()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks().Take(10).ToList());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(8, best.Movies.Count());
+		}
+
+		[TestMethod]
+		public void MoviePickerUnboundedKnapsack_ChooseBest_WeekEnding_20170604()
+		{
+			var test = ConstructTestObject();
+			var movies = new List<IMovie>();
+			int id = 1;
+
+			// FML 
+			movies.Add(ConstructMovie(id++, "Wonder Woman", 103, 845));
+			movies.Add(ConstructMovie(id++, "Captain Underpants", 23.9m, 239));
+			movies.Add(ConstructMovie(id++, "Pirates", 22.1m, 193));
+			movies.Add(ConstructMovie(id++, "Guardians", 9.8m, 74));
+			movies.Add(ConstructMovie(id++, "Baywatch", 8.7m, 62));
+			movies.Add(ConstructMovie(id++, "Alien", 4.1m, 31));
+			movies.Add(ConstructMovie(id++, "Everything Everything", 3.3m, 22));
+			movies.Add(ConstructMovie(id++, "Diary of a Wimpy Kid", 1.3m, 17));
+			movies.Add(ConstructMovie(id++, "Snatched", 1.3m, 14));
+			movies.Add(ConstructMovie(id++, "King Arthur", 1.2m, 12));
+
+			test.AddMovies(movies);
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(1, best.Movies.Count(movie => movie.Name == "Wonder Woman"));
+			Assert.AreEqual(7, best.Movies.Count(movie => movie.Name == "Everything Everything"));
+		}
+
+		[TestMethod]
+		public void MoviePickerUnboundedKnapsack_ChooseBest_WeekEnding_20170611()
+		{
+			var test = ConstructTestObject();
+			var movies = new List<IMovie>();
+			int id = 1;
+
+			// Weekend gross from box office mojo (with FML bux)
+ 
+			movies.Add(ConstructMovie(id++, "Wonder Woman", 58520672m, 613));
+			movies.Add(ConstructMovie(id++, "The Mummy", 31688375m, 526));
+			movies.Add(ConstructMovie(id++, "It Comes at night", 5988370m, 150));
+			movies.Add(ConstructMovie(id++, "Pirates of the caribbean", 10704103m, 143));
+			movies.Add(ConstructMovie(id++, "Baywatch", 4648207m, 60));
+			movies.Add(ConstructMovie(id++, "Megan Leavey", 3810867m, 59));
+			movies.Add(ConstructMovie(id++, "Alien: Covenant", 1826579m, 26));
+			movies.Add(ConstructMovie(id++, "My Cousin Rachel", 968506m, 15));
+			movies.Add(ConstructMovie(id++, "Diary of a wimpy Kid", 656843m, 8));
+			movies.Add(ConstructMovie(id++, "Captain Underpants", 12180704m, 198));
+			movies.Add(ConstructMovie(id++, "Everything, Everything", 1627295m, 28));
+			movies.Add(ConstructMovie(id++, "Guardians of the Galaxy", 6312367m, 70));
+			movies.Add(ConstructMovie(id++, "King Arthur", 455744m, 7));
+			movies.Add(ConstructMovie(id++, "Snatched", 491644m, 9));
+			movies.Add(ConstructMovie(id++, "Best of the rest", 512445m, 9));
+
+			test.AddMovies(movies);
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+
+			Assert.AreEqual(1, best.Movies.Count(movie => movie.Name == "Wonder Woman"));
+			Assert.AreEqual(5, best.Movies.Count(movie => movie.Name == "Guardians of the Galaxy"));
+			Assert.AreEqual(1, best.Movies.Count(movie => movie.Name == "Alien: Covenant"));
+			Assert.AreEqual(1, best.Movies.Count(movie => movie.Name == "Diary of a wimpy Kid"));
+		}
+
+        [TestMethod]
+        public void MoviePickerUnboundedKnapsack_ChooseBest_WeekEnding_20170618()
+        {
+            var test = ConstructTestObject();
+            var movies = new List<IMovie>();
+            int id = 1;
+
+            // Weekend gross from box office mojo (with FML bux)
+
+            movies.Add(ConstructMovie(id++, "Cars 3", 53.68868m, 719));
+            movies.Add(ConstructMovie(id++, "Wonder Woman", 41.26885m, 478));
+            movies.Add(ConstructMovie(id++, "All Eyez on Me", 26.435354m, 327));
+            movies.Add(ConstructMovie(id++, "Rough Night", 8.004283m, 243));
+            movies.Add(ConstructMovie(id++, "The Mummy", 14.51096m, 167));
+            movies.Add(ConstructMovie(id++, "47 Meters Down", 11.205561m, 105));
+            movies.Add(ConstructMovie(id++, "Captain Underpants", 7.181541m, 78));
+            movies.Add(ConstructMovie(id++, "Pirates of the caribbean", 8.964039m, 71));
+            movies.Add(ConstructMovie(id++, "Guardians of the Galaxy", 5.071732m, 60));
+            movies.Add(ConstructMovie(id++, "It Comes at night", 2.601468m, 34));
+            movies.Add(ConstructMovie(id++, "The Book of Henry", 1.42454m, 31));
+            movies.Add(ConstructMovie(id++, "Baywatch", 1.610987m, 29));
+            movies.Add(ConstructMovie(id++, "Megan Leavey", 2.541644m, 25));
+            movies.Add(ConstructMovie(id++, "Alien: Covenant", 0.568149m, 11));
+            movies.Add(ConstructMovie(id++, "Everything, Everything", 0.356241m, 10));
+
+            test.AddMovies(movies);
+
+            var best = test.ChooseBest();
+
+            WritePicker(test);
+            WriteMovies(best);
+
+            Assert.AreEqual(1, best.Movies.Count(movie => movie.Name == "Wonder Woman"));
+            Assert.AreEqual(7, best.Movies.Count(movie => movie.Name == "Pirates of the caribbean"));
+       }
+
+        [TestMethod]
+		public void MoviePickerUnboundedKnapsack_ChooseBest_ThisWeeksPicks()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks());
+
+			var best = test.ChooseBest();
+
+			WritePicker(test);
+			WriteMovies(best);
+		}
+
+		[TestMethod]
+		public void MoviePickerUnboundedKnapsack_ChooseBest_Top10_ThisWeeksPicks()
+		{
+			var test = ConstructTestObject();
+
+			test.AddMovies(ThisWeeksMoviesPicks());
+
+			var best = test.ChooseBest(10);
+
+			WritePicker(test);
+
+			foreach (var movieList in best)
+			{
+				Logger.WriteLine(string.Empty);
+				WriteMovies(movieList);
+			}
+		}
+
+		//----==== PRIVATE ====---------------------------------------------------------
+
+		private List<IMovie> ThisWeeksMoviesPicks()
+		{
+			var movies = new List<IMovie>();
+
+			int id = 1;
+
+			movies.Add(ConstructMovie(id++, "Fifty Shades Freed", 38.4m, 556));
+			movies.Add(ConstructMovie(id++, "Peter Rabbit", 18.7m, 276));
+			movies.Add(ConstructMovie(id++, "The 1517 to Paris", 15.1m, 221));
+			movies.Add(ConstructMovie(id++, "Jumanji Welcome to the Jungle", 7.2m, 126));
+			movies.Add(ConstructMovie(id++, "The Greatest Showman", 6m, 101));
+			movies.Add(ConstructMovie(id++, "Maze Runner The Death Cure", 5.1m, 83));
+			movies.Add(ConstructMovie(id++, "Winchester", 4m, 66));
+			movies.Add(ConstructMovie(id++, "The Shape of Water", 3.3m, 51));
+			movies.Add(ConstructMovie(id++, "The Post", 3.2m, 50));
+			movies.Add(ConstructMovie(id++, "Hostiles", 2.7m, 46));
+			movies.Add(ConstructMovie(id++, "12 Strong", 2.6m, 43));
+			movies.Add(ConstructMovie(id++, "Den of Thieves", 2.3m, 40));
+			movies.Add(ConstructMovie(id++, "Three Billboards Outside Ebbing Missouri", 2.2m, 35));
+			movies.Add(ConstructMovie(id++, "I Tonya", 1.8m, 30));
+			movies.Add(ConstructMovie(id++, "Darkest Hour", 1.7m, 29));
+
+			return movies;
+		}
+	}
+}
---
+++ MoviePicker/MoviePickerUnboundedKnapsack.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MoviePicker.Common.Interfaces;
+
+namespace MooveePicker
+{
+	/// <summary>
+	/// Attempting to solve the problem using Unbounded Knapsack which is a variation on the 0-1 Knapsack but you have unlimited copies of an item.
+	/// Use some of the dominance relations:
+	///		Modular - Using the greatest density of value (most value for the cost - highest value/cost)
+	///		
+	/// REF: https://en.wikipedia.org/wiki/Knapsack_problem
+	///		
+	/// </summary>
+	public class MoviePickerUnboundedKnapsack : IMoviePicker
+	{
+		private const int MAX_MOVIES = 15;
+
+		private readonly List<IMovie> _movies;              // The list of baseline movies.
+		private readonly IMovieList _movieListPrototype;
+
+		// Store the already processed sub-problems
+		private readonly HashSet<int> _processedHashes;
+
+		private IMovie _bestPerformer;
+		private List<IMovie> _bestPerformers;
+		private bool _enableBestPerformer;
+		private IMovie[] _movieArray;
+		private int _topCount;
+		private List<IMovieList> _topLists;
+		private decimal _topMin;                        // The minimum of the list is only calculated when items are added/updated (not ALL the time)
+
+		public MoviePickerUnboundedKnapsack(IMovieList movieListPrototype)
+		{
+			_enableBestPerformer = true;
+			_movieArray = new IMovie[MAX_MOVIES];
+			_movies = new List<IMovie>();
+			_processedHashes = new HashSet<int>();
+
+			_movieListPrototype = movieListPrototype;
+
+			ScreenCount = 8;
+			TotalCost = 1000;
+		}
+
+		/// <summary>
+		/// Best Performer will return a value ONLY if it is the BEST performer (and there are no ties)
+		/// </summary>
+		public IMovie BestPerformer => _bestPerformer != null && _bestPerformers == null ? _bestPerformer : null;
+
+		/// <summary>
+		/// A list of TIED Best Performers.
+		/// </summary>
+		public IEnumerable<IMovie> BestPerformers => _bestPerformers;
+
+		public bool EnableBestPerformer
+		{
+			get
+			{
+				return _enableBestPerformer;
+			}
+			set
+			{
+				_enableBestPerformer = value;
+
+				if (_enableBestPerformer)
+				{
+					// Re-add the movies.
+				}
+				else
+				{
+					_bestPerformer = null;
+					_bestPerformers = null;
+
+					foreach (var movie in _movies)
+					{
+						movie.IsBestPerformer = false;
+					}
+				}
+			}
+		}
+
+		public IEnumerable<IMovie> Movies => _movies;
+
+		public int ScreenCount { get; set; }
+
+		public int TotalCost { get; set; }
+
+		public int TotalComparisons { get; set; }
+
+		public int TotalSubProblems => _processedHashes.Count;
+
+		public void AddMovies(IEnumerable<IMovie> movies)
+		{
+			_movies.Clear();
+
+			foreach (var movie in movies)
+			{
+				_movies.Add(movie);
+			}
+
+			if (EnableBestPerformer)
+			{
+				_bestPerformer = null;
+				_bestPerformers = null;
+
+				foreach (var movie in _movies.OrderByDescending(item => item.Efficiency))
+				{
+					if (_bestPerformer == null)
+					{
+						// Assign the first one as the best performer.
+
+						_bestPerformer = movie;
+						movie.IsBestPerformer = true;
+					}
+					else if (_bestPerformer.Efficiency == movie.Efficiency)
+					{
+						// Check to see if there are MANY tied Best Performers
+
+						if (_bestPerformers == null)
+						{
+							_bestPerformers = new List<IMovie> { _bestPerformer };
+						}
+
+						movie.IsBestPerformer = true;
+						_bestPerformers.Add(movie);
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+
+			if (_bestPerformers != null)
+			{
+				_bestPerformer = null;      // There is NO one best performer.
+			}
+		}
+
+		public IMovieList ChooseBest()
+		{
+			IMovieList best = _movieListPrototype.Clone();
+			var idx = 0;
+			var maxMovies = _movies.Count;
+
+			TotalComparisons = 0;
+			_processedHashes.Clear();
+
+			foreach (var movie in _movies)
+			{
+				_movieArray[idx++] = movie;
+			}
+
+			// Create all 15 ^ 8 possibilities. (Kinda brute force, but it's worth a try.)
+			// Recursive calls COULD be slow, but investigate that later.
+			// Trying to also minimize the memory footprint.
+			// Figure out how to use Dominance Relations to clip out some of the noise.
+
+			var topCineplexes = new int[_topCount == 0 ? 1 : _topCount, 9];      // Store the top cineplexes (8 screen IDs)
+			var currentCineplex = new int[9];
+
+			for (int cineIdx = 0; cineIdx < currentCineplex.Length; cineIdx++)
+			{
+				currentCineplex[cineIdx] = -1;
+			}
+
+			for (int dim1 = 0; dim1 < maxMovies; dim1++)
+			{
+				currentCineplex[0] = dim1;
+				currentCineplex[8] = (int)(_movieArray[dim1]?.Earnings ?? 0m);
+
+				// Only adding one movie shouldn't break the bank,
+				// so you shouldn't have to check to see if it is over the TotalCost
+
+				for (int dim2 = 0; dim2 < maxMovies; dim2++)
+				{
+					var totalCost2 = _movieArray[dim1]?.Cost + _movieArray[dim2]?.Cost;
+
+					if (totalCost2 < TotalCost)
+					{
+						for (int dim3 = 0; dim3 < maxMovies; dim3++)
+						{
+							var totalCost3 = totalCost2 + _movieArray[dim3]?.Cost;
+
+							if (totalCost3 < TotalCost)
+							{
+								// Keep adding dimensions... 8 screens
+
+								if (_movieArray[dim1]?.Earnings + _movieArray[dim2]?.Earnings + _movieArray[dim3]?.Earnings > _topMin)
+								{
+
+								}
+							}
+							else
+							{
+								// Can't add any more.
+							}
+						}
+					}
+					else if (currentCineplex[8] > _topMin)
+					{
+						for (int cineIdx = 0; cineIdx < currentCineplex.Length; cineIdx++)
+						{
+							topCineplexes[0, cineIdx] = currentCineplex[cineIdx];
+						}
+
+						_topMin = currentCineplex[8];
+					}
+				}
+			}
+
+			// Add the top cineplex to the result.
+
+			for (int cineIdx = 0; cineIdx < currentCineplex.Length - 1; cineIdx++)
+			{
+				var movie = topCineplexes[0, cineIdx] >= 0 ? _movieArray[topCineplexes[0, cineIdx]] : null;
+
+				if (movie != null)
+				{
+					best.Add(movie);
+				}
+			}
+
+			return best;
+		}
+
+		public List<IMovieList> ChooseBest(int topCount)
+		{
+			_topLists = new List<IMovieList>();
+			_topCount = topCount;
+
+			ChooseBest();
+
+			return _topLists.OrderByDescending(list => list.TotalEarnings).ToList();
+		}
+
+		public void Clear()
+		{
+			_enableBestPerformer = true;
+			_bestPerformers = null;
+			_movies.Clear();
+			_processedHashes.Clear();
+		}
+
+		//----==== PRIVATE ====----------------------------------------------------------------------
+
+		private decimal ReplaceTopMin(int[,] topCineplexes, int[] replaceWithCineplex)
+		{
+			// Find the smallest value in top cineplexes.
+
+			// Then replace that cineplex
+
+			// Find the newest smallest (which could be the one coming in)
+
+			return 0m;
+		}
+	}
+}
