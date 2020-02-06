627b3c3fa37ff8645aa931373a1c2d61cfb27b81
Parker Smart <psmart@spilledmilk.com>
2020-02-06 16:55:57 -0700

    Updated movie name comparison a bit.

--- MoviePicker.Tests/MovieTests.cs
+++ MoviePicker.Tests/MovieTests.cs
@@ -90,5 +90,23 @@ namespace MoviePicker.Tests
 
 			Assert.IsFalse(movie1.Equals(movie2), "The movie names equal");
 		}
+
+		[TestMethod, TestCategory("Mock")]
+		public void Movie_Equals_Case_Insensitive_Match_With_Noise_At_End()
+		{
+			var movie1 = new Movie { Name = "Birds of Prey and the Fantabulous Emancipation of One Harley Quinn" };
+			var movie2 = new Movie { Name = "Birds of Prey" };
+
+			Assert.IsTrue(movie1.Equals(movie2), "The movie names aren't equal");
+		}
+
+		[TestMethod, TestCategory("Mock")]
+		public void Movie_Equals_Case_Insensitive_Match_With_Noise_At_End2()
+		{
+			var movie1 = new Movie { Name = "Birds of Prey" };
+			var movie2 = new Movie { Name = "Birds of Prey and the Fantabulous Emancipation of One Harley Quinn" };
+
+			Assert.IsTrue(movie1.Equals(movie2), "The movie names aren't equal");
+		}
 	}
 }
