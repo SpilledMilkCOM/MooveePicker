15fa644fed9676aa36cdcf7365e4964f80e2933b
Parker Smart <psmart@spilledmilk.com>
2020-03-05 20:59:02 -0700

    Adding B, P emojis

--- Common.Emoji/EmojiConstants.cs
+++ Common.Emoji/EmojiConstants.cs
@@ -6,9 +6,12 @@
 		// FFR: (for future reference) when adding an emoji in code, you should probably have some text that describes the emoji,
 		//		because HOW the hell are you going to SEARCH for the emoji when you need to FIND it!!.
 		// OR...  Just put them into a Constants class.
+		// REF: https://emojis.wiki/p-button/  Provides copy function
 
+		public const string GREEN_B_BUTTON = "🅱️";
+		public const string BLUE_P_BUTTON = "🅿️";	//U+1F17F 
 		public const string GREEN_HEART = "💚";		//U+1F49A
 		public const string NECKTIE = "👔";			//U+1F454
-		public const string REMINDER = "►"; 
+		public const string REMINDER = "►";
 	}
 }
